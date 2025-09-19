608d13dbb33bf00684c1507880b8b326a3eb3aa9
Rora Berry <136183954+roraspberry@users.noreply.github.com>
2025-09-19 21:42:38 +0100

    Implement session package with simple structs and unit tests.

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"scrum-poker-go/internal/session"
+)
 
 func main() {
-	fmt.Println("Hello, Go + Docker + GCP!")
+	s := session.NewSession("Sprint 1 Planning")
+	p, _ := s.AddPlayer("Big Tuna")
+	fmt.Printf("Created session %s with player %s\n", s.Title, p.Name)
 }
